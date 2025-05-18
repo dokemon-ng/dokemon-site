630e8f2993416679fbd68bbeeff93dd2d9656a66
Albert Einstein <javastraat@hotmail.com>
2025-05-18 23:51:37 +0200

    fixed some things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -200,7 +200,7 @@ services:
           <li>
             <a
               className="flex items-center gap-2 text-sm"
-              href="https://github.com/productiveops/dokemon/issues"
+              href="https://github.com/dokemon-ng/Software/issues"
               target="_blank"
             >
               <FontAwesomeIcon
