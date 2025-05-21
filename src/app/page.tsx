97d042b7588643457ab58f8ff5653a7d50a01cb2
Albert Einstein <javastraat@hotmail.com>
2025-05-21 16:20:10 +0200

    little fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -283,9 +283,8 @@ services:
                 </li>
                 <li className="text-center">
                   <h4 className="text-lg font-semibold">Why is it named Dokémon?</h4>
-		{/* eslint-disable-next-line react/no-unescaped-entities */}
 		<p className="text-base">Docker Manager → DockMan → DocMan → Dokémon<br />
-		  That's how we arrived at the name! Doesn't really mean anything.</p>
+		  That&apos;s how we arrived at the name! Doesn&apos;t really mean anything.</p>
 		</li>
               </ul>
             </div>
