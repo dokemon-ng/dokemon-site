4f486bb0c432d02f6f3af3023d4e5502aeb1a656
Albert Einstein <javastraat@hotmail.com>
2025-05-21 19:36:57 +0200

    add dokemon website sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,6 +148,15 @@ services:
         image: openspeedtest/latest
 `;
 
+  const dokemonsiteCompose = `
+  dokemon-site:
+    image: javastraat/dokemon-site:latest
+    container_name: dokemon-site
+    ports:
+      - "3000:3000"
+    restart: unless-stopped
+`;
+
 
 
 
@@ -249,6 +258,27 @@ services:
           <div className="flex flex-col items-center">
             <h2 className="text-2xl font-bold mb-8 text-center">Compose Samples</h2>
 
+            {/* Dokemon Site Sample Section */}
+            <div className="w-full mb-12 px-4 md:px-0">
+              <h3 className="text-xl font-bold mb-4 text-center">Dokemon Website</h3>
+              <div className="mb-4 w-full">
+                <pre className="bg-slate-800 p-4 text-sm font-mono overflow-x-auto rounded">
+                  {dokemonsiteCompose}
+                </pre>
+              </div>
+              <div className="mb-8 flex justify-center">
+                <button 
+                  type="button"
+                  className="w-24 rounded-md bg-amber-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
+                  onClick={() => {
+                    navigator.clipboard.writeText(openspeedtestCompose);
+                  }}
+                >
+                  Copy
+                </button>
+              </div>
+            </div>
+
             {/* PiHole Sample Section */}
             <div className="w-full mb-12 px-4 md:px-0">
               <h3 className="text-xl font-bold mb-4 text-center">PiHole - Network-wide Ad Blocking</h3>
@@ -269,6 +299,7 @@ services:
                 </button>
               </div>
             </div>
+
             {/* Openspeedtest Sample Section */}
             <div className="w-full mb-12 px-4 md:px-0">
               <h3 className="text-xl font-bold mb-4 text-center">OpenSpeedTest</h3>
